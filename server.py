a91663019004a27736f8b6ef8f0c5773b4b460be
Kevin TianXiang Wu <kevintxwu@gmail.com>
2017-02-23 22:06:30 -0800

    Jimmy Neutron

---
+++ server.py
@@ -0,0 +1,39 @@
+from flask import Flask, request, redirect
+import redis
+
+app = Flask(__name__) 
+chars = [str(x) for x in range(10)] + [chr(x) for x in range(97, 123)]
+r     = redis.StrictRedis(host='localhost', port=6379, db=0)
+
+html  = '''
+<style>
+body{font-family:monospace;}
+</style>
+<p>asd.fyi - share text.</p>
+<form action="/" method="post"><textarea name="t" rows="20" cols="80"></textarea><br/><br/><input type="submit" /></form>
+'''
+
+def getid():
+	return r.incr("id", 1)
+
+def n_to_s(n):
+	s = ""
+	while n:
+		s = s + chars[n % len(chars)]
+		n = n // len(chars)
+	return s
+
+@app.route("/", defaults={"path": ""}, methods=["POST", "GET"])
+@app.route("/<path>")
+def paste(path):
+	if path:
+		return "<pre>%s</pre>" % (r.get(path).decode("utf-8"),)
+	elif request.form.get("t"):	
+		t = request.form.get("t")
+		curid = n_to_s(getid())
+		r.set(curid, t)
+		return redirect("/%s" % (curid,))
+	return html
+
+if __name__ == '__main__':
+	app.run()
